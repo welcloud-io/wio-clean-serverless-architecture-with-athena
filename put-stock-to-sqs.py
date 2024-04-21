06d1c7548b32f21412b87de9cfb1df40c564ae7e
Olivier Lemaitre <o.lemaitre@welcloud.io>
2024-04-21 15:15:12 +0000

    simplify put stock script

--- put-stock-to-sqs.py
+++ put-stock-to-sqs.py
@@ -2,7 +2,6 @@ import boto3
 import time
 import datetime
 import random
-from tqdm import tqdm
 
 sqs = boto3.client('sqs')
 sts = boto3.client('sts')
@@ -10,18 +9,15 @@ sts = boto3.client('sts')
 def current_aws_account_number():
     return sts.get_caller_identity().get('Account')
 
-for stock_event_number in tqdm(range(3000)):
+for stock_event_number in range(3000):
     stock_id = random.choice(['A', 'B', 'C']) + str(random.randint(1, 5)) * 3
     stock_level = random.randint(2, 100)
     line = f"{stock_id};{stock_level}"
 
-    try:
-        response = sqs.send_message(
-            QueueUrl = f'https://sqs.eu-west-1.amazonaws.com/{current_aws_account_number()}/CleanServerlessQueue',
-            MessageBody = (line)
-        )
-    except:
-        pass
+    response = sqs.send_message(
+        QueueUrl = f'https://sqs.eu-west-1.amazonaws.com/{current_aws_account_number()}/CleanServerlessQueue',
+        MessageBody = (line)
+    )
     
-    # print(stock_event_number, response)
+    print(stock_event_number, response)
     #time.sleep(0.0005)
